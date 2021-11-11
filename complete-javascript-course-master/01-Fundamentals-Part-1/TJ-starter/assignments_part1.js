bec48a9ef4a9633590d9e913d4439a88eb81c854
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2021-11-11 10:02:04 -0500

    Update: Completed Coding Challenge 1 and created new folder for challenges

--- complete-javascript-course-master/01-Fundamentals-Part-1/TJ-starter/assignments_part1.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/TJ-starter/assignments_part1.js
@@ -62,64 +62,31 @@ console.log(population > 6000000);
 console.log(population < 33000000);
 
 // 5. Based on the variables you created, create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'
-let description = 'The ' + country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+let description = 'The ' + country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language +'.';
 
-console.log(description);
+console.log('Concatenanted string: ' + description);
 
 
 /*
-* CODING CHALLENGE # 1
+* LECTURE: Strings and Template Literals
 */
 
-// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
-// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
-// Your tasks:
-// 1. Store Mark's and John's mass and height in variables
-let markMass;
-let johnMass;
-let markHeight;
-let johnHeight;
+//1. Recreate the 'description' variable from the last assignment, this time using the template literal syntax
+description = `The ${country} is in ${continent}, and its ${population} people speak ${language}.`;
 
-// 2. Calculate both their BMIs using the formula (you can even implement both versions)
+console.log('String template literal: ' + description);
 
-// let markBMI = markMass/markHeight ** 2
-// let johnBMI = johnMass/(johnHeight * johnHeight);
- 
-// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 
-// let markHigherBMI = markBMI > johnBMI;
+/*
+* LECTURE: Taking Decisions: if / else Statements
+*/
 
-// Test data:
-// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+// 1. If your country's population is greater than 33 million, log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
 
-markMass = 78;
-markHeight = 1.69;
+if (population > 33000000){
+  console.log(`The ${country}'s population is above average.`);
+} else {
+  console.log(`The ${country}'s population is ${33000000 - population} below average`)  
+}
+// 2. Aftercheckingtheresult,changethepopulationtemporarilyto13andthento 130. See the different results, and set the population back to original
 
-johnMass = 92;
-johnHeight = 1.95;
-
-let markBMI = markMass/markHeight ** 2
-let johnBMI = johnMass/(johnHeight * johnHeight);
-
-console.log('mark\'s BMI: ' + markBMI);
-console.log('john\'s BMI: ' + johnBMI);
-
-let markHigherBMI = markBMI > johnBMI;
-console.log('mark\'s BMI is higher: ' + markHigherBMI);
-
-// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
-
-markMass = 95;
-markHeight = 1.88;
-
-johnMass = 85;
-johnHeight = 1.76;
-
-markBMI = markMass/markHeight ** 2
-johnBMI = johnMass/(johnHeight * johnHeight);
-
-console.log('mark\'s BMI: ' + markBMI);
-console.log('john\'s BMI: ' + johnBMI);
-
-markHigherBMI = markBMI > johnBMI;
-console.log('mark\'s BMI is higher: ' + markHigherBMI);
