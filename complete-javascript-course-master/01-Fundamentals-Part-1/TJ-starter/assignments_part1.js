61440086992b66d5263015134ec02a204bbc5e08
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2021-11-11 21:14:07 -0500

    Update: Completed LECTURE: The Conditional (Ternary) Operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/TJ-starter/assignments_part1.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/TJ-starter/assignments_part1.js
@@ -222,4 +222,14 @@ console.log('*******************************************************************
 */
 
 // 1. If your country's population is greater than 33 million, use the ternary operator to log a string like this to the console: 'Portugal's population is above average'. Otherwise, simply log 'Portugal's population is below average'. Notice how only one word changes between these two sentences!
+
+
+console.log(`The ${country}'s population is ${population > 33000000 ? 'above' : 'below'} average`);
+
 // 2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original. 
+population = 13000000
+console.log(`Population changed to: ${population}`);
+
+console.log(`The ${country}'s population is ${population > 33000000 ? 'above' : 'below'} average`);
+
+console.log('**************************/  END OF PART /****************************')
--- complete-javascript-course-master/01-Fundamentals-Part-1/TJ-starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/TJ-starter/script.js
@@ -17,3 +17,12 @@ console.log(firstName);
 * LECTURE: The Conditional (Ternary) Operator
 */
 
+const age = 34;
+
+// age >= 18 ? console.log('I like to drink wine') :
+// console.log("I'll have water.");
+
+const drink = age >= 18 ? 'drink wine' : 'drink water';
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
