9ac30f89ff4e230d8f5bb206713c9ed1af1bfb1e
Tiffini Johnson <86614374+tjohnson7021@users.noreply.github.com>
2021-11-15 10:59:06 -0500

    w.i.p - Dot vs. Bracket Notation

--- complete-javascript-course-master/02-Fundamentals-Part-2/TJ-starter/assignments_part2.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/TJ-starter/assignments_part2.js
@@ -169,3 +169,68 @@ neighbors[indexOfMexico] = "Republic of Mexico";
 
 console.log(neighbors);
 
+
+
+console.log('*******************************************************************');
+/*
+* LECTURE: Introduction to Objects
+*/
+
+// 1. Create an object called 'myCountry' for a country of your choice, containing properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments).
+
+const myCountry = {
+    country: "USA",
+    capital: "Wasington DC",
+    language: "English",
+    population: 335000000,
+    neighbors: ["Canada", "Mexico"],
+
+    describe: function() {
+        return console.log(`The ${this.country} has ${this.population/1000000} million ${this.language}-speaking people, ${this.neighbors.length} neighbouring countries and a capital called ${this.capital}.`)
+
+    }
+
+    checkIsland: function() {
+
+        this.push('isIsland: ')
+        if(this.neighbors.length > 0){
+
+        }
+    }
+}
+
+console.log(myCountry);
+
+
+console.log('*******************************************************************');
+/*
+* LECTURE: Dot vs. Bracket Notation
+*/
+
+// 1. Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.'.
+
+console.log(`The ${myCountry.country} has ${myCountry.population/1000000} million ${myCountry.language}-speaking people, ${myCountry.neighbors.length} neighbouring countries and a capital called ${myCountry.capital}.`)
+
+// 2. Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation.
+
+myCountry.population = myCountry.population + 2000000;
+console.log(myCountry.population);
+
+myCountry["population"] = myCountry["population"] - 2000000;
+console.log(myCountry["population"]);
+
+
+
+console.log('*******************************************************************');
+/*
+* LECTURE: Object Methods
+*/
+
+// 1. Add a method called 'describe' to the 'myCountry' object. This method will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword.
+
+// 2. Call the 'describe' method.
+
+myCountry.describe();
+
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This method will set a new property on the object, called 'isIsland'. 'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.
+
