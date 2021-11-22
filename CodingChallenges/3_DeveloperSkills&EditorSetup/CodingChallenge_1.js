997ece6cb3e7ce2e936e25e353997b35cf34132c
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2021-11-22 17:51:47 -0500

    Update: Fixed directory for coding challenges

---
+++ CodingChallenges/3_DeveloperSkills&EditorSetup/CodingChallenge_1.js
@@ -0,0 +1,17 @@
+'use strict';
+/*
+activating strict mode makes the code more secure. So again, first strict mode forbids us to do certain things and second it creates visible errors and the developer console, where in other situations JavaScript would just fail silently.
+*/
+
+/*
+* CODING CHALLENGE # 1: Developer Skills & Editor Setup:
+*/
+
+
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures. Example: [17, 21, 23] will print "... 17°C in 1 days ... 21°C in 2 days ... 23°C in 3 days ..."
+// Your tasks:
+// 1. Createafunction'printForecast'whichtakesinanarray'arr'andlogsa string like the above to the console. Try it with both test datasets.
+// 2. Usetheproblem-solvingframework:Understandtheproblemandbreakitup into sub-problems!
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data2:[12,5,-5,0,4]
