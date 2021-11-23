1d9f3e867b459776fec232ddc68fcdcb7e560991
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2021-11-22 19:10:19 -0500

    Update: Completed CC for Developer Skills and Editor Setup

--- CodingChallenges/3_DeveloperSkills&EditorSetup/CodingChallenge_1.js
+++ CodingChallenges/3_DeveloperSkills&EditorSetup/CodingChallenge_1.js
@@ -10,8 +10,22 @@ activating strict mode makes the code more secure. So again, first strict mode f
 
 // Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures. Example: [17, 21, 23] will print "... 17°C in 1 days ... 21°C in 2 days ... 23°C in 3 days ..."
 // Your tasks:
-// 1. Createafunction'printForecast'whichtakesinanarray'arr'andlogsa string like the above to the console. Try it with both test datasets.
-// 2. Usetheproblem-solvingframework:Understandtheproblemandbreakitup into sub-problems!
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console. Try it with both test datasets.
+// 2. Use the problem-solving framework: Understand the problem and breakitup into sub-problems!
+
+
 // Test data:
 // § Data 1: [17, 21, 23]
-// § Data2:[12,5,-5,0,4]
+// § Data 2: [12,5,-5,0,4]
+
+function printForecast(arr){
+  let str = "";
+  for(let i = 0; i < arr.length; i++){
+    str += `... ${arr[i]}°C in ${i + 1} days `;
+  }
+  console.log(str);
+}
+
+printForecast([17, 21, 23]);
+
+printForecast([12,5,-5,0,4]);
