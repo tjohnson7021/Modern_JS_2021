f43aee8d93dde8e269d79acce1eb40a02798e4a6
Tiffini Johnson <86614374+tjohnson7021@users.noreply.github.com>
2022-01-20 14:39:16 -0500

    Update: W.I.P roll dice button functionality

--- complete-javascript-course-master/07-Pig-Game/TJ - starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/TJ - starter/script.js
@@ -3,8 +3,42 @@
 // Selecting elements from the html
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceElement = document.querySelector('.dice');
 
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+
+
+// Starting conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
-diceElement.classList.add('hidden')
+diceElement.classList.add('hidden');
+
+
+//Rolling dice functinality
+btnRoll.addEventListener('click', function () {
+    // 1. Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    // 2. Display dice
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${dice}.png`;
+
+    // 3. Check for if a '1' is rolled: if true, 
+
+    if(dice != 1){
+        // Add dice to current score 
+        currentScore += dice;
+        current0El.textContent = currentScore; //CHANGE LATER
+
+    } else {
+        //switch to next player
+    }
+    
+});
