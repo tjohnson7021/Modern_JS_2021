f60002744a772d315f378dbc2bacc163057b97a9
Tiffini Johnson <86614374+tjohnson7021@users.noreply.github.com>
2022-01-20 14:00:47 -0500

    Update: Started Pig Game Project

--- complete-javascript-course-master/07-Pig-Game/TJ - starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/TJ - starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting elements from the html
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden')
