c31631d6143936dbad81a5406fd11b7eabfd3305
Tiffini Johnson <86614374+tjohnson7021@users.noreply.github.com>
2022-01-21 11:45:30 -0500

    Update: W.I.P Hold Button functionality

--- complete-javascript-course-master/07-Pig-Game/TJ - starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/TJ - starter/script.js
@@ -1,9 +1,14 @@
 'use strict';
 
 // Selecting elements from the html
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
+//player 1
 const current0El = document.getElementById('current--0');
+//player 2
 const current1El = document.getElementById('current--1');
 
 const diceElement = document.querySelector('.dice');
@@ -12,8 +17,17 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
+};
 
 // Starting conditions
 score0Element.textContent = 0;
@@ -25,7 +39,7 @@ diceElement.classList.add('hidden');
 btnRoll.addEventListener('click', function () {
     // 1. Generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+
     // 2. Display dice
     diceElement.classList.remove('hidden');
     diceElement.src = `dice-${dice}.png`;
@@ -35,10 +49,30 @@ btnRoll.addEventListener('click', function () {
     if(dice != 1){
         // Add dice to current score 
         currentScore += dice;
-        current0El.textContent = currentScore; //CHANGE LATER
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
     } else {
         //switch to next player
+        switchPlayer();
+
+        
     }
     
+});
+
+
+btnHold.addEventListener('click', function () {
+    
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    // Finish the game
+
+    //Switch to the next player
+    switchPlayer();
+
 });
