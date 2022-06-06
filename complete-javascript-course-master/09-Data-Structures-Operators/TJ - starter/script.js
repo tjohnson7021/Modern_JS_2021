d70bf60011d3d342664d861ebefe705a0909458f
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2022-06-06 09:42:23 -0400

    Section-9 Completed: Rest Pattern and Parameters
    - completed Rest Pattern and Parameters
    - moving on to Short Circuiting (&& and ||)

--- complete-javascript-course-master/09-Data-Structures-Operators/TJ - starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/TJ - starter/script.js
@@ -46,6 +46,11 @@ const restaurant = {
 
   orderPasta: function(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3} `);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients){
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   }
 };
 
@@ -67,15 +72,50 @@ console.log('*** THE REST OPERATOR ***');
 /*** THE REST OPERATOR ***/
 
 /**
- * The SPREAD operator works on the RIGHT side: 
+ * The SPREAD operator works on the RIGHT side:
  */
 const arr =[1,2,...[3,4]];
 
 /**
- * The REST operator works on the LEFT side: 
+ * The REST operator works on the LEFT side:
  */
 const [a,b,...others] = [1,2,3,4,5];
 console.log(a,b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza, risotto, otherFood);
+
+/**
+ * Objects (Rest Pattern)
+ */
+
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+
+
+/**
+ *  2) Functions (Rest Pattern)
+ */
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x); // adds all the values in x, whatever they are.
+
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
 
 // console.log('*** THE SPREAD OPERATOR ***');
 // /*** THE SPREAD OPERATOR ***/
